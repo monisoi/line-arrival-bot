dcd51d86850e63c80ecad4dfeed75faf9e2f73d0
ichii nakamura <ichii.nakamura@gmail.com>
2018-02-24 21:40:05 +0900

    fix typo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import express from 'express';
-import { LineBotMiddleware } from './line-bot';
-import { handleEvent } from './handle-event';
+import { lineBotMiddleware } from './line-bot';
+import handleEvent from './handle-event';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// const handleEvent = () => {
-
-// }
-
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/webhook', LineBotMiddleware, (req, res) => {
+app.post('/webhook', lineBotMiddleware, (req, res) => {
   Promise.all(req.body.events.map(handleEvent)).then(result => res.json(result));
 });
 
