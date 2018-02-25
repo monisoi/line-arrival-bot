e216535be7d6140e9db71c4f1b83e97f35b1f096
ichii nakamura <ichii.nakamura@gmail.com>
2018-02-25 10:03:12 +0900

    modify deolocation request options

--- src/fetch-geolocation.js
+++ src/fetch-geolocation.js
@@ -2,9 +2,17 @@ import request from 'request-promise-native';
 
 export default async () => {
   const key = process.env.GEOLOCATION_API_KEY;
-  const uri = `https://www.googleapis.com/geolocation/v1/geolocate?key=${key}`;
+  // const uri = `https://www.googleapis.com/geolocation/v1/geolocate?key=${key}`;
+  const options = {
+    method: 'POST',
+    uri: `https://www.googleapis.com/geolocation/v1/geolocate?key=${key}`,
+    json: true,
+    headers: {
+      Authorization: 'application/json',
+    },
+  };
   try {
-    const body = await request({ uri, json: true });
+    const body = await request(options);
     const { location } = body;
     return location;
   } catch (e) {
